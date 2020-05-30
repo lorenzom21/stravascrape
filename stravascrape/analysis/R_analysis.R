d4fbbe0d7791b1a0fece41790f5226645ec1b5b5
Lorenzo M <lorenzom21@gmail.com>
2020-05-30 10:23:48 -0400

    updated R analysis

--- stravascrape/analysis/R_analysis.R
+++ stravascrape/analysis/R_analysis.R
@@ -2,6 +2,7 @@ setwd("~/Google Drive/NYCDSA/webscraping/stravascrape_data")
 
 library(lubridate)
 library(dplyr)
+library(ggplot2)
 
 data_df = read.csv('joined_df.csv', stringsAsFactors = F)
 data_df$start_date = mdy(data_df$start_date)
@@ -18,13 +19,21 @@ data_df$ft_per_mile=data_df$elevation/data_df$distancedf2
 
 #classify based on kJ
 data_df$kJ_cat = "no cat"
-data_df$kJ_cat[data_df$kJ<=500] = "A) below 500 kJ"
-data_df$kJ_cat[500 < data_df$kJ & data_df$kJ<=1000] = "B) 500 to 1000 kJ"
-data_df$kJ_cat[1000 < data_df$kJ & data_df$kJ<=1500] = "C) 1,000 to 1,500 kJ"
-data_df$kJ_cat[1500 < data_df$kJ] = "D) greater than 1,500 kJ"
+data_df$kJ_cat[data_df$kJ<=250] = "A) up to 250 kJ"
+data_df$kJ_cat[250 < data_df$kJ & data_df$kJ<=750] = "B) 250 to 750 kJ"
+data_df$kJ_cat[750 < data_df$kJ & data_df$kJ<=1250] = "C) 1,000 to 1,500 kJ"
+data_df$kJ_cat[1250 < data_df$kJ] = "D) greater than 1,250 kJ"
 data_df$kJ_cat = as.factor(data_df$kJ_cat)
 data_df$year = as.factor(year(data_df$start_date))
 summary(data_df$kJ_cat)
+
+#find max watts
+max(data_df[data_df$start_date>as.Date('2017-09-02'),c('max_watts')],na.rm=T) #1493
+data_df %>% filter(.,max_watts==1493) #1400063224
+
+#total distance
+sum(data_df$distancedf2) #15249.32
+
 
 g_base = ggplot(data=data_df)
 
@@ -32,8 +41,13 @@ g_base = ggplot(data=data_df)
 hist(data_df$kJ)
 g_base + geom_histogram(aes(x=kJ),binwidth=500)
 
+#distance and elevation within the kJ_cat groups
 data_df %>% group_by(kJ_cat) %>%
-  summarise(.,mean_distance = mean(distancedf2, na.rm=T),sd_distance = sd(distancedf2, na.rm=T),mean_elevation = mean(elevation,na.rm=T),sd_elevation = sd(elevation,na.rm=T))
+  summarise(.,n=n(),mean_distance = mean(distancedf2, na.rm=T),sd_distance = sd(distancedf2, na.rm=T),mean_elevation = mean(elevation,na.rm=T),sd_elevation = sd(elevation,na.rm=T))
+
+#kJ cat vs distance
+g_base + geom_bar(aes(x=kJ_cat))
+g_base+geom_boxplot(aes(x=kJ_cat,y=distancedf2))
 
 #Plot distance vs kJ
 ggplot(data=data_df,aes(x=distancedf2,y=kJ)) + geom_point(aes(color=watts_type))  + geom_smooth(method='lm') + labs(x="distance in miles", y="Energy in kJ", color='power data source')
@@ -41,17 +55,18 @@ ggplot(data=data_df,aes(x=distancedf2,y=kJ)) + geom_point(aes(color=watts_type))
 #avg weighted power over time by kJ grouping
 ggplot(data=data_df, aes(x=start_date,y=avg_weighted_watts))+geom_point(aes(color=watts_type)) + geom_smooth(method='lm') + facet_wrap(~kJ_cat) +labs(x='date', y='avg weighted power', color='power data source')
 
-g_base + geom_bar(aes(x=kJ_cat))
-g_base+geom_boxplot(aes(x=kJ_cat,y=distancedf2))
-
-#Calories over time
-g_base+geom_point(aes(x=start_date,y=Calories, col=watts_type)) + labs(x='date', y='Calories', color='power data source')
-ggplot(data=data_df,aes(x=Calories)) +geom_histogram(binwidth=100) + facet_wrap(~year)
-g_base + geom_boxplot(aes(x=kJ_cat,y=Calories))
 
 #histogram of rides by year
 ggplot(data=data_df,aes(x=year))+geom_bar()
 
-#slope by year
-ggplot(data=data_df[data_df$ft_per_mile<150,],aes(x=ft_per_mile))+geom_histogram(binwidth=5) +facet_wrap(~year) +xlab("feet climb per mile travelled")
+#Calories historgram
+ggplot(data=data_df,aes(x=Calories)) +geom_histogram(binwidth=500,aes(fill=start_day))
+
+
+#cadence histogram
+ggplot(data = data_df[data_df$avg_cad>60,], aes(x=avg_cad)) + geom_histogram(binwidth=5) + ylab("test")
+
+
+
+
 
