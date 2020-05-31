fa191e37c317204bbd75d31f44744dd9e6278f81
Lorenzo M <lorenzom21@gmail.com>
2020-05-31 00:39:05 -0400

    Improved analysis R

--- stravascrape/analysis/R_analysis.R
+++ stravascrape/analysis/R_analysis.R
@@ -36,10 +36,11 @@ sum(data_df$distancedf2) #15249.32
 
 
 g_base = ggplot(data=data_df)
+font_size = theme(text = element_text(size=20))
 
 #historgram of kJ
-hist(data_df$kJ)
-g_base + geom_histogram(aes(x=kJ),binwidth=500)
+
+g_base + geom_histogram(aes(x=kJ),binwidth=500) + font_size
 
 #distance and elevation within the kJ_cat groups
 data_df %>% group_by(kJ_cat) %>%
@@ -53,18 +54,28 @@ g_base+geom_boxplot(aes(x=kJ_cat,y=distancedf2))
 ggplot(data=data_df,aes(x=distancedf2,y=kJ)) + geom_point(aes(color=watts_type))  + geom_smooth(method='lm') + labs(x="distance in miles", y="Energy in kJ", color='power data source')
 
 #avg weighted power over time by kJ grouping
-ggplot(data=data_df, aes(x=start_date,y=avg_weighted_watts))+geom_point(aes(color=watts_type)) + geom_smooth(method='lm') + facet_wrap(~kJ_cat) +labs(x='date', y='avg weighted power', color='power data source')
+ggplot(data=data_df, aes(x=start_date,y=avg_weighted_watts))+geom_point(aes(color=watts_type)) + geom_smooth(method='lm') + facet_wrap(~kJ_cat) +labs(x='date', y='avg weighted power', color='power data source') + font_size
 
+#boxplot by year
+ggplot(data=data_df, aes(x=year,y=avg_weighted_watts)) + geom_boxplot()+ geom_jitter(shape=1, position=position_jitter(0.2)) + facet_wrap(~kJ_cat) +labs(x='years', y='avg weighted power') + font_size
+
+unique_days = data_df %>% group_by (start_date) %>% summarise(n=n())
+
+unique_days$year = year(unique_days$start_date)
+unique_days
+
+#histogram of ride days per year
+ggplot(data=unique_days,aes(x=year))+geom_bar() + font_size
 
 #histogram of rides by year
-ggplot(data=data_df,aes(x=year))+geom_bar()
+ggplot(data=data_df,aes(x=year))+geom_bar() + font_size
 
 #Calories historgram
-ggplot(data=data_df,aes(x=Calories)) +geom_histogram(binwidth=500,aes(fill=start_day))
+ggplot(data=data_df,aes(x=Calories)) +geom_histogram(binwidth=500,aes(fill=start_day)) + labs(fill='day') + font_size
 
 
 #cadence histogram
-ggplot(data = data_df[data_df$avg_cad>60,], aes(x=avg_cad)) + geom_histogram(binwidth=5) + ylab("test")
+ggplot(data = data_df[data_df$avg_cad>60,], aes(x=avg_cad)) + geom_histogram(binwidth=5) + ylab("test") + font_size
 
 
 
